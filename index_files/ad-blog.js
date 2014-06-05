2e7f1227d2970ff8a0a6bb4732147173fecf0db5
ice110956 <494267437@qq.com>
2014-06-05 14:08:59 +0800

    initial commit

---
+++ index_files/ad-blog.js
@@ -0,0 +1,28 @@
+﻿var ad_type = 'ms809';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+
+ ad_type = 'ms812_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/**/
+ ad_type = 'ms815_3';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/**/
+ ad_type = 'ms819_4';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
